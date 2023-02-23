9002fd11a4139410ab04b582bac4549f253b5928
linuxmint <linuxmint@linuxmint21.linuxvmimages.local>
2023-02-23 11:21:08 -0500

    Test de GetInstance

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -5,9 +5,16 @@ package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
 
+import java.util.logging.Logger;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
 
 /**
  * @author alumno
@@ -31,14 +38,33 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+	 * 	public static ReusablePool getInstance(){
+		if (instance == null)
+			instance = new ReusablePool(2);
+		return instance; 
+	}
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
-	}
+		//fail("Not yet implemented");
+		  ReusablePool pool;
+		  pool = ReusablePool.getInstance();
+		  assertNotEquals(pool, null);
+		  }
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+	 * 	public Reusable acquireReusable() throws NotFreeInstanceException{
+		if (reusables.size()>0){
+			Reusable r=(Reusable)reusables.lastElement();
+			reusables.remove(r);
+			return r;			
+		}
+		else{
+			throw(new NotFreeInstanceException());
+		}
+
+	}
 	 */
 	@Test
 	public void testAcquireReusable() {
@@ -47,6 +73,15 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 	
+	 	public void releaseReusable(Reusable r) throws DuplicatedInstanceException {
+		if (reusables.contains(r)==false){
+			reusables.add(r);
+		}
+		else{
+			throw(new DuplicatedInstanceException());
+		}
+	}
 	 */
 	@Test
 	public void testReleaseReusable() {
