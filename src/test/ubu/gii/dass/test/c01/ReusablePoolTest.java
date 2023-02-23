29c7427b14fea63d399ca72a40d7d7e19f676341
sdg1002 <74184248+sdg1002@users.noreply.github.com>
2023-02-23 17:38:47 +0100

    Update ReusablePoolTest.java

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -34,7 +34,11 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		  ReusablePool pool;
+		  pool = ReusablePool.getInstance();
+		  assertNotEquals(pool, null);
+		  
 	}
 
 	/**
