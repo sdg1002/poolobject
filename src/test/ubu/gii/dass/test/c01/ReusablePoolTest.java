8a52b6190cf8e18e393236a7243892b404dd22e7
sdg1002 <74184248+sdg1002@users.noreply.github.com>
2023-02-26 13:00:33 +0100

    Actualización TestAdquireReusable

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -72,22 +72,41 @@ public class ReusablePoolTest {
 	}
 
 	/**
+	 * 
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
-	 * 	
+	 * 
+	 * @throws NotFreeInstanceException
+	 * 
 	 */
+
 	@Test
+
 	public void testAcquireReusable() {
-		ReusablePool pool = ReusablePool.getInstance(); 
-		//
-		Reusable r1,r2,r3;
+
+		r1 = null;
+		r2 = null;
+		r3 = null;
+
+		assertEquals(r1, null);
+		assertEquals(r2, null);
+		assertEquals(r3, null);
+
 		try {
-			r1 = pool.acquireReusable();
-			r2 = pool.acquireReusable();
-			r3 = pool.acquireReusable();
-		}catch(Exception NotFreeInstanceException){
-			System.out.println("Todo correcto");
+			r1 = pool1.acquireReusable();
+			r2 = pool1.acquireReusable();
+			assertTrue(r1 instanceof Reusable);
+			assertTrue(r2 instanceof Reusable);
+
+		} catch (Exception NotFreeInstanceException) {
+			fail(" No se adquieren correctamente los reutilizables");
 		}
-		
+
+		try {
+			r3 = pool1.acquireReusable();
+		} catch (Exception NotFreeInstanceException) {
+			System.out.print(" Test AcquireReusable se realiza correctamente");
+		}
+
 	}
 
 	/**
@@ -110,7 +129,7 @@ public class ReusablePoolTest {
 		    r1 = pool2.acquireReusable();
 			r1 = pool2.acquireReusable();
 		    }catch(Exception DuplicatedInstanceException){
-		    	System.out.println("Todo correcto");
+		    	System.out.println("Test ReleaseReusable se realiza correctamente");
 		    }
 	}
 }
